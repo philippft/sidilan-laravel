8ac884964bb6f17e001beb607d01cc7f72370ecb
philippft <dzosfphilip@gmail.com>
2025-11-24 11:22:50 +0800

    Add Admin Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+     public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+ 
+        return back()->withErrors([
+            'Username' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+}
