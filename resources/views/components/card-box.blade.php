f18531f89aa9e83ffb4a9a4250c0d7f94f47e9cf
YasMbul.com <deasmeranggi55@gmail.com>
2025-12-16 02:39:21 +0800

    feat: install chartjs library, create cardbox, add label on textField

---
+++ resources/views/components/card-box.blade.php
@@ -0,0 +1,3 @@
+<div {{-- class=""  --}} {{ $attributes->merge(['class' => 'py-4 px-5 shadow-xl rounded-2xl']) }}>
+   {{ $slot }}
+</div>
--- resources/views/components/textField.blade.php
+++ resources/views/components/textField.blade.php
@@ -12,4 +12,5 @@
    @else
       <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}"
          {{ $attributes->merge(['class' => 'mt-2 w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent']) }}>
+   @endif
 </div>
