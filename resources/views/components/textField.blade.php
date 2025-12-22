b35158074302d7e26c9bdb4fe5ad1e9b8d3f5640
YasMbul.com <deasmeranggi55@gmail.com>
2025-12-23 00:26:49 +0800

    kelar buat grafik

--- resources/views/components/textField.blade.php
+++ resources/views/components/textField.blade.php
@@ -1,17 +1,17 @@
 @props(['id', 'label', 'name', 'type' => 'text'])
 
 <div>
-   <label for="{{ $id }}" class="font-medium text-xl">
+   <label for="{{ $id }}" class="font-medium text-xl mb-2">
       {{ $label }}
    </label>
    @if ($type == 'textarea')
       <textarea name="{{ $name }}" id="{{ $id }}"
-         {{ $attributes->merge(['class' => 'mt-2 w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent']) }}>
+         {{ $attributes->merge(['class' => 'w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent']) }}>
          {{ $slot }}
       </textarea>
    @else
       <input type="{{ $type }}" name="{{ $name }}" id="{{ $id }}"
-         {{ $attributes->merge(['class' => 'mt-2 w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent']) }}>
+         {{ $attributes->merge(['class' => 'w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent']) }}>
    @endif
 
 </div>
