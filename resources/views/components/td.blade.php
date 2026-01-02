ea65c0190dd0ac67999bd159473925a6c6507ad2
GoesDey <gusdekmh7778@gmail.com>
2026-01-03 02:29:10 +0800

    style: buat responsive sidebar, tambah edit data, management data

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -1,3 +1,3 @@
-<td {{ $attributes->merge(['class' => "py-2 font-medium text-dongker-sidilan"])}}>
+<td {{ $attributes->merge(['class' => "py-2 text-base font-medium text-dongker-sidilan"])}}>
      {{ $slot }}
 </td>
--- resources/views/components/textField.blade.php
+++ resources/views/components/textField.blade.php
@@ -1,16 +1,16 @@
 @props(['id', 'label', 'name', 'value' => null,'placeholder', 'type' => 'text'])
 
-<div class="mb-2">
-   <label for="{{ $id }}" class="font-medium text-xl mb-2 block">
+<div class="lg:mb-3 mb-1.5">
+   <label for="{{ $id }}" class="font-medium md:text-xl text-base md:mb-2 mb-1 block">
       {{ $label }}
    </label>
    @if ($type == 'textarea')
       <textarea value="{{ old($name, $value) }}" placeholder="{{ $placeholder }}" name="{{ $name }}" id="{{ $id }}"
-         {{ $attributes->merge(['class' => 'w-full px-3 py-2 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:ring-inset focus:border-transparent']) }}>
+         {{ $attributes->merge(['class' => 'md:text-base text-xs w-full md:px-3 px-2 md:py-2 py-1.5 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:ring-inset focus:border-transparent']) }}>
          {{ $slot }}
       </textarea>
       @else
       <input value="{{ old($name, $value) }}" type="{{ $type }}" name="{{ $name }}" id="{{ $id }}" placeholder="{{ $placeholder }}"
-      {{ $attributes->merge(['class' => 'w-full px-3 py-3 border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-dongker-sidilan focus:ring-inset focus:border-transparent']) }}>
+      {{ $attributes->merge(['class' => 'md:text-base text-xs w-full md:px-3 px-2 py-2  border bg-white border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-dongker-sidilan focus:ring-inset focus:border-transparent']) }}>
     @endif
 </div>
