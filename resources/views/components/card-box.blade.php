afbc559340a87f165862e7602bfa88189debd3e4
YasMbul.com <deasmeranggi55@gmail.com>
2026-01-04 23:26:47 +0800

    ini kena prettier ku jadi keubah

--- resources/views/components/card-box.blade.php
+++ resources/views/components/card-box.blade.php
@@ -1,5 +1,5 @@
 @props(['animasi' => 'transition-all duration-300 hover:-translate-y-2 hover:shadow-xl'])
 
-<div {{-- class="shadow-md" --}} {{ $attributes->merge(['class' => 'py-4 px-5 shadow-md rounded-2xl ' . $animasi]) }}>
+<div {{-- class="shadow-md" --}} {{ $attributes->merge(['class' => 'py-4 px-5 shadow-lg rounded-2xl ' . $animasi]) }}>
    {{ $slot }}
 </div>
